e815cf7b18d736a8c185509f9aa4992ed2c16264
proav2323 <108835201+proav2323@users.noreply.github.com>
2024-03-26 16:53:43 -0500

    coding

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   presets: [require("@spartan-ng/ui-core/hlm-tailwind-preset")],
-  content: [
-    "./src/**/*.{html,ts}",
-    "./ui-button-helm/**/*.{html,ts}",
-    "./ui-icon-helm/**/*.{html,ts}",
-    "./ui-dialog-helm/**/*.{html,ts}",
-    "./ui-input-helm/**/*.{html,ts}",
-    "./ui-label-helm/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}", ".libs/ui/**/*.{html,ts}"],
   theme: {
     extend: {},
   },
